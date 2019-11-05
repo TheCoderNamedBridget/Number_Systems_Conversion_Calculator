45f4d90757f6945fe750877c4ddf89840dd57727
Bridget <bridgetnaylor2000@gmail.com>
2019-11-05 10:56:56 -0800

    Initial commit

---
+++ app/src/main/java/com/example/conversioncalculatordecimalbinaryhexidecimaloctal/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.conversioncalculatordecimalbinaryhexidecimaloctal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void conversionLogicStreams(){
+        
+    }
+
+    public void binToDeci(){
+
+    }
+
+    public void deciToBin(){
+
+    }
+
+    public void differentBaseToDeci(String numberSystem, String number){
+
+    }
+
+    public void binToHexOrOct(){
+
+    }
+}
